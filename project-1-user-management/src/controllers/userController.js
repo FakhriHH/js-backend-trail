deb1f9a21078440447550aa1532edffc8135cda9
FakhriHH <fakhrihabib9@gmail.com>
2024-12-27 14:32:43 +0700

    Enkripsi password dan pendaftaran

--- project-1-user-management/src/controllers/userController.js
+++ project-1-user-management/src/controllers/userController.js
@@ -1,10 +1,15 @@
 const knex = require('../database/knex');
+const bcrypt = require('bcrypt');
 
 // Create User
 exports.createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const [id] = await knex('users').insert({ name, email, password });
+
+    // Enkripsi password sebelum disimpan
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [id] = await knex('users').insert({ name, email, password: hashedPassword });
     res.status(201).json({ id, message: 'User created successfully' });
   } catch (error) {
     res.status(500).json({ error: 'Failed to create user', details: error.message });
